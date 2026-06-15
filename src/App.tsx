81fb8043af893787f747d2f18276cb8c375d2868
AdrienRCQ <aricque@PW0F809J.GROUP.HARDIS>
2026-06-15 18:02:56 +0200

    Ajout d'une nouvelle page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Servers from "./pages/Servers";
+import About from "./pages/About";
 
 function App() {
   return (
-    <>
-      <Servers />
-    </>
+    <BrowserRouter>
+      {/* MENU */}
+      <nav className="navbar">
+        <h2>MyApp</h2>
+
+        <div className="nav-links">
+          <Link to="/servers" className="nav-link">
+            Servers
+          </Link>
+          <Link to="/about" className="nav-link">
+            About
+          </Link>
+        </div>
+      </nav>
+
+      {/* ROUTES */}
+      <Routes>
+        <Route path="/" element={<Servers />} />
+        <Route path="/servers" element={<Servers />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<h1>Page not found</h1>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/About.tsx
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>À propos</h1>
+      <p>Ceci est une seconde page de ton application React.</p>
+    </div>
+  );
+}
